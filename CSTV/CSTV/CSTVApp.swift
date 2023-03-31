5a9e9fc71cfcbfe9702d4240ef00e420d855afe9
victoraujo <victor.vieira@dtidigital.com.br>
2023-03-31 18:39:22 -0300

    create project

---
+++ CSTV/CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by Victor Vieira on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
